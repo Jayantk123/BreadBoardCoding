19899076aa8a6366fec662aa5bbea47586b269a2
Jayant <58224270+Jayantk123@users.noreply.github.com>
2021-09-12 00:34:13 +0530

    Update _dbconnect.php

--- loginsystem/partials/_dbconnect.php
+++ loginsystem/partials/_dbconnect.php
@@ -1,8 +1,19 @@
+// <?php 
+
+// $db_user = "epiz_29669398";
+// $db_pass = "g6BvpWTOSSrfc4o";
+// $db_name = "epiz_29669398_users";
+
+// $db = new PDO('mysql:host=sql208.epizy.com;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
+// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// ?>
+
 <?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "users";
+$server = "sql208.epizy.com";
+$username = "epiz_29669398";
+$password = "g6BvpWTOSSrfc4o";
+$database = "epiz_29669398_users";
 
 $conn = mysqli_connect($server, $username, $password, $database);
 if (!$conn){
